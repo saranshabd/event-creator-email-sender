35d2390ad564bc0fa17cfc64f2109218b89a0b96
saran-shabd <shabd.179301181@muj.manipal.edu>
2019-08-20 19:50:09 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,83 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
+const fs = require('fs')
+
+// load configurations from environment
+require('dotenv').config()
+
+const app = express()
+
+// use body-parser middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+/**
+ * utility method to check if there are any empty strings
+ */
+function containsEmptyString(args) {
+  for (let i = 0; i < args.length; ++i)
+    if (undefined === args[i] || '' === args[i]) return true
+  return false
+}
+
+/**
+ * route to send emails to service providers regarding new registered events
+ */
+app.post('/event-creator/service-provider/send-email', (request, response) => {
+  const { date, title, venue, handler, handlerEmail } = request.body
+
+  // check for invalid arguments
+  if (containsEmptyString([date, title, venue, handler, handlerEmail]))
+    return response
+      .status(400)
+      .json({ status: false, message: 'invalid arguments' })
+
+  // get email sender configurations
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    secureConnection: false,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
+    },
+    tls: {
+      ciphers: 'SSLv3'
+    }
+  })
+
+  // get email template
+  let template = fs.readFileSync('./emails/template.html').toString()
+
+  // fill the template with real values
+  template = template.replace('@@name@@', title)
+  template = template.replace('@@date@@', date)
+  template = template.replace('@@venue@@', venue)
+  template = template.replace('@@serviceprovider@@', handler)
+
+  // send email using the above template
+  transport
+    .sendMail({
+      from: 'Mext',
+      to: handlerEmail,
+      subject: 'Event Notification - Mext',
+      html: template
+    })
+    .then(() => {
+      response
+        .status(200)
+        .json({ status: true, message: 'email sent successfully' })
+    })
+    .catch(error => {
+      console.log(error)
+      response
+        .status(500)
+        .json({ status: false, message: 'internal server error' })
+    })
+})
+
+const port = process.env.PORT || 2323
+app.listen(port, () => console.log(`server is up and running at port ${port}`))
